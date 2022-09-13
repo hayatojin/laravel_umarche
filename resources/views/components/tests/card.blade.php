acebd3cb3bb94a637d5185b4545f4aa61071d774
jin <jihakeno21@gmail.com>
2022-09-13 16:10:26 -0700

    属性バッグ

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,4 +1,12 @@
-<div class = "border-2 shadow-md w-1/4 p-2">
+@props([
+    'title', // 初期値を設定しないパターン
+    'message' => '初期値です',
+    'content' => '本文の初期値です'
+])
+
+<div {{ $attributes->merge([
+    'class' => 'border-2 shadow-md w-1/4 p-2'
+]) }} >
     <div>{{ $title }}</div>
     <div>画像</div>
     <div>{{ $content }}</div>
