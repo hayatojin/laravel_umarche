10fa4e4c035e046cede6ea052e484132f8ea188c
jin <jihakeno21@gmail.com>
2022-09-13 23:59:19 -0700

    マルチログイン

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,9 +3,14 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Route;
 
 class Authenticate extends Middleware
 {
+    protected $user_route = 'user.login';
+    protected $owner_route = 'owner.login';
+    protected $admin_route = 'admin.login';
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
@@ -14,8 +19,15 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
+        // ownerのつくURLにアクセスがあり、ログインしていない場合は、オーナーのログイン画面へ飛ばす + adminの場合、userの場合についても記載
         if (! $request->expectsJson()) {
-            return route('login');
+            if(Route::is('owner.*')){
+                return route($this->owner_route);
+            } else if(Route::is('Admin.*')) {
+                return route($this->admin_route);
+            } else {
+                return route($this->user_route);
+            }
         }
     }
 }
