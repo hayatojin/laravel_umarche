13a31de7d633da40378c245b25f7d130dcfec896
jin <jihakeno21@gmail.com>
2022-09-16 19:36:50 -0700

    middleware, abort(404)

---
+++ app/Http/Controllers/Owner/ShopController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Owner;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Shop;
+
+class ShopController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        // 他のユーザーの編集画面を見れなくする
+        $this->middleware(function ($request, $next) {
+            // dd($request->route());
+
+            // ShopオーナーのIDと、ログインIDを比較（合っていればOK・違ったらエラー表示）
+            $id = $request->route()->parameter('shop'); //shopのid取得
+                if(!is_null($id)){ // null判定
+                    $shopsOwnerId = Shop::findOrFail($id)->owner->id;
+                    $shopId = (int)$shopsOwnerId; // キャスト 文字列→数値に型変換 
+                    $ownerId = Auth::id();
+
+                if($shopId !== $ownerId){ // 同じでなかったら
+                    abort(404); // 404画面表示 }
+                    }
+                }
+                return $next($request);
+        });
+    }
+
+    public function index()
+    {
+        $ownerId = Auth::id();
+        $shops = Shop::where('owner_id', $ownerId)->get();
+
+        return view('owner.shops.index', compact('shops'));
+    }
+
+    // edit/{shop}の{}には、オーナーIDが入ってくる（オーナー側のルーティングによって呼び出される）
+    // ルーティングからパラメータを取得している
+    public function edit($id)
+    {
+        dd(Shop::findOrFail($id));
+    }
+
+    public function update(Request $request, $id)
+    {
+    }
+}
