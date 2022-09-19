e819674d09700a080cebc216d4c05b1678f59aa0
jin <jihakeno21@gmail.com>
2022-09-18 17:30:04 -0700

    shop,edit,update

--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -9,6 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <x-flash-message status="session('status')" />
                     @foreach ( $shops as $shop)
                         <div class="w-1/2 p-4">
                         {{-- edit画面へアクセスする時に、shopのIDもパラメータで渡す --}}
@@ -23,14 +24,7 @@
                                 @endif
                             </div>
                             <div class="text-xl">{{ $shop->name }}</div>
-                            <div>
-                                @if(empty($shop->filename))
-                                  <img src="{{ asset('images/no_image.jpg') }}">
-                                @else
-                                  <img src="{{ asset('strage/shops/' . $shop->filename) }}">
-                                  {{-- アップロードされた画像は「strage」に保存される --}}
-                                @endif
-                            </div>
+                            <x-shop-thumbnail :filename="$shop->filename" /> {{-- ショップ画像のあり or なしをコンポーネント化しているため、それを使う --}}
                         </div>
                         </a>
                         </div>
