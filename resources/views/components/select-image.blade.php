08562025bed4dbb12b706fd5c44fecc2167d897c
jin <jihakeno21@gmail.com>
2022-09-20 14:52:17 -0700

    product create

--- resources/views/components/select-image.blade.php
+++ resources/views/components/select-image.blade.php
@@ -3,10 +3,11 @@
     if( $name === 'image2'){ $modal = 'modal-2'; }
     if( $name === 'image3'){ $modal = 'modal-3'; }
     if( $name === 'image4'){ $modal = 'modal-4'; }
+    if( $name === 'image5'){ $modal = 'modal-5'; }
 @endphp
 
 <div class="modal micromodal-slide" id="{{ $modal }}" aria-hidden="true">
-    <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+    <div class="modal__overlay z-50" tabindex="-1" data-micromodal-close>
       <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="{{ $modal }}-title">
         <header class="modal__header">
           <h2 class="text-xl text-gray-700" id="modal-1-title">
@@ -14,6 +15,7 @@
           </h2>
           <button type="button" class="modal__close" aria-label="Close modal" data-micromodal-close></button>
         </header>
+
         <main class="modal__content" id="{{ $modal }}-content">
             <div class="flex flex-wrap">
                 @foreach ( $images as $image)
@@ -30,18 +32,19 @@
                 @endforeach
             </div>
         </main>
+
         <footer class="modal__footer">
           <button type="button" class="modal__btn" data-micromodal-close aria-label="閉じる">閉じる</button>
         </footer>
+
       </div>
     </div>
   </div>
 
   <div class="flex justify-around items-center mb-4">
-    <a data-micromodal-trigger="{{ $modal }}" href='javascript:;'>ファイルを選択</a>
-    <div class="w-1/4">
-    <img id="{{ $name }}_thumbnail" src="">
+    <a class="py-2 px-4 bg-gray-200" data-micromodal-trigger="{{ $modal }}" href='javascript:;'>ファイルを選択</a>
+        <div class="w-1/4">
+            <img id="{{ $name }}_thumbnail" src="">
+        </div>
     </div>
-   </div>
-   <input id="{{ $name}}_hidden" type="hidden"
-   name="{{ $name }}" value=""> 
+   <input id="{{ $name }}_hidden" type="hidden" name="{{ $name }}" value="">
